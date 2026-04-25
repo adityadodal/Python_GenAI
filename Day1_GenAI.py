648b41e4bf241597203511023b86acae49ea2efb
Aditya Dodal <adityadodal@gmail.com>
2026-04-26 05:17:22 +0530

    Check

--- Day1_GenAI.py
+++ Day1_GenAI.py
@@ -1,4 +1,10 @@
 # In this session we will learn about GenAI in Python
 #
+
+# We will learn how to use already existing llm and also hpw to fine tune the response according to our requirements
 #
+
+#
+
+
 #
