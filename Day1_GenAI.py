f84942beb0c3efc316b335615fd4eba8020d9960
Aditya Dodal <adityadodal@gmail.com>
2026-04-26 05:45:01 +0530

    Excecuted First GenAI Program Sucessfully

--- Day1_GenAI.py
+++ Day1_GenAI.py
@@ -8,3 +8,10 @@
 
 
 #
+from transformers import pipeline
+
+generator = pipeline("text-generation", model="gpt2")
+
+output = generator("Artificial Intelligence is",
+                   max_new_tokens=40, num_return_sequences=1)
+print(output[0]['generated_text'])
